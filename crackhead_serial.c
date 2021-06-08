480f64fe4c96774c22e9d3fd7943cedb59b28150
zohairhadi <zohairabbashadi@hotmail.com>
2021-06-08 12:42:23 +0500

    i hate c

--- crackhead_serial.c
+++ crackhead_serial.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+
 #ifdef __linux__ 
     //linux code goes here
     #include <crypt.h>
@@ -12,7 +14,7 @@
 #define FILE_PATH = "etc/shadows"
 
 char* readFile(char* filename){
-    printf("Runnuing ->readFile...\n");
+    printf("Runnuing ->readFile...\n\n");
 
     char ch;
     char* toReturn = 0;
@@ -35,8 +37,10 @@ char* readFile(char* filename){
     int c = 0;
     if (toReturn)
     {
-        while((ch = fgetc(fp)) != EOF)
+        while((ch = fgetc(fp)) != EOF){
             toReturn[c++] = ch;
+                // reads text until newline is encountered
+        }
     }
 
     fclose(fp);
@@ -44,19 +48,80 @@ char* readFile(char* filename){
 }
 
 
-char* extractHashedPassword(char user_name[25]){
-    printf("Runnuing ->extractHasedPassword...\n");
+int cmp_str(char* array1, char* array2){
+    printf("Comparing:%s-%s-%li-%li\n", array1, array2, strlen(array1), strlen(array2));
+    int s = strlen(array1);
+    for (int i=0; i < s; i++){
+        printf("HELLL\n");
+        printf("%i\n", array1[i] != '\n');
+        if (array1[i] != '\n'){
+            if (array1[i] != array2[i]){
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+
+char* extractHashedPassword(char* user_name){
+    printf("Running ->extractHasedPassword...\n");
     // read all file data
     char* file_details = readFile("/etc/shadow");
     
-    printf("%s", file_details);
-    // loop over data
-    // read line by line anc comapre
+    // printf("%s", file_details);
+    char* name_array = (char*)malloc(256*sizeof(char));
+    char* password = (char*)malloc(256*sizeof(char));
+    int temp_counter = 0; int file_counter = 0; 
 
-    // if line matches user_name, extract password
-    // return hashed password
 
-    // return null if not found
+    char* token = strtok(file_details, "\n");
+    while (token) {
+    // token[strlen(token)] = '\0';
+        token = token + '\0';
+        printf("%s-%s-%i-%li-%li\n", token, user_name, cmp_str(user_name, token), strlen(token), strlen(user_name));
+
+        if (strncmp(user_name, token, 6) == 0){
+            printf("Nigger");
+        }
+
+        // if (cmp_str(user_name, token)){
+        //     token = strtok(NULL, ":");
+        //     printf("Password Mastiii: %s\n", token);
+        //     return token;
+        // }
+        token = strtok(NULL, ":");
+    }
+
+
+    // while (file_details[file_counter] != NULL) {
+    //     temp_counter = 0;
+    //     name_array = (char*)malloc(256*sizeof(char));
+
+    //     name_array[temp_counter] = file_details[file_counter];
+    //     temp_counter += 1;
+    //     file_counter += 1;
+    //     printf("%c", name_array[temp_counter-1]);
+    //     if (name_array[temp_counter] == ':'){
+    //         printf(name_array);
+    //         name_array[5] = '\0';
+    //         printf(name_array);
+
+    //         int is_same = cmp_str(user_name, name_array);
+    //         printf("SHUGAL MELA: %i\n", is_same);
+    //         return NULL;
+
+    //         if (is_same == 1){
+    //             temp_counter = 0;
+    //             file_counter += 1;
+    //             while(file_details[file_counter] != ':')
+    //                 password[temp_counter++] = file_details[file_counter++];
+                
+    //             return password;
+    //         } 
+    //         else temp_counter = 0;
+    //     }
+    // }
+    // return NULL;
 }
 
 
@@ -64,7 +129,7 @@ int main(void)
 {
     // a password string we'll randomly guess
     char id[] = "mpiuser";
-    char user_name[25];
+    char* user_name = (char*)malloc(25*sizeof(char));
 
     // a random string?
     char salt[] = "$6$4GfdWqHx$";
@@ -75,6 +140,13 @@ int main(void)
     printf("Enter username\n");
     fgets(user_name, 25, stdin);
 
+
+    // user_name[strlen(user_name) - 1] = '\0';
+ 
+    // printf("", strlen(user_name))
+ 
+    // printf("2. %s----%li\n", user_name, strlen(user_name));
+
     // get hased password from file
-    extractHashedPassword(user_name);
+    (extractHashedPassword(user_name));
 }
